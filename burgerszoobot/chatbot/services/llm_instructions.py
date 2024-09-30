4a55655790ce79fbaa04e503f8e0f989de2cd1eb
Wout Gijsbers <wout_gijsbers@hotmail.com>
2024-09-30 17:16:43 +0200

    Update llm_instructions.py
    - removed old instructions

--- burgerszoobot/chatbot/services/llm_instructions.py
+++ burgerszoobot/chatbot/services/llm_instructions.py
@@ -12,14 +12,6 @@ INSTRUCTIONS_RETRIEVAL_WITH_EXAMPLE_ANSWER = textwrap.dedent("""
     You are a helpful guide in Burgers' Zoo. You will be given a question from a visitor. Return an example answer that may be found on the official Burgers' Zoo website.
 """)
 
-_INSTRUCTIONS_RETRIEVAL_WITH_SUBQUESTIONS = textwrap.dedent("""
-    You are a helpful guide in Burgers' Zoo. You will be given a question from a visitor.
-    Suggest up to five additional related questions to help them find the information they need, for the provided question.
-    Suggest only short questions without compound sentences. Suggest a variety of questions that cover different aspects of the topic.
-    Make sure each question answers a very specific topic, and that they are related to the original question.
-    Output one question per line. Do not number the questions.      
-""")
-
 INSTRUCTIONS_RETRIEVAL_WITH_SUBQUESTIONS = textwrap.dedent("""
     You are an LLM that's part of a RAG system that answers questions of visitors to Burgers' Zoo in Arnhem, The Netherlands. You will be given a question as input.
     Suggest up to five additional related questions to help find the information needed to answer the provided question.
