930ecb04bd25ca7c3b7bf7bfda7aedc8b625531f
Wout Gijsbers <wout_gijsbers@hotmail.com>
2024-12-24 14:55:15 +0100

    set gpt-4o-mini as default model

--- burgerszoobot/chatbot/services/llm_interface.py
+++ burgerszoobot/chatbot/services/llm_interface.py
@@ -9,10 +9,10 @@ OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 client = openai.OpenAI(api_key=OPENAI_API_KEY)
 
 
-def query_gpt(messages: list[dict[str, str]], temperature: float = 0.7, top_p: float = 0.95) -> str:
+def query_gpt(messages: list[dict[str, str]], model: str = "gpt-4o-mini", temperature: float = 0.7, top_p: float = 0.95) -> str:
     response = client.chat.completions.create(
         messages=messages,  # type: ignore
-        model="gpt-4o-2024-08-06",
+        model=model,
         temperature=temperature,
         top_p=top_p
     )
